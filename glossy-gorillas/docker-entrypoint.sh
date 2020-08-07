bc5d84b5e80b0a0d45342d3048e9255b6a052ffa
Rafael Cortês <mrfyda@gmail.com>
2020-08-07 21:56:32 +0100

    Serve assets in production mode

--- glossy-gorillas/docker-entrypoint.sh
+++ glossy-gorillas/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+python manage.py collectstatic --no-input
 python manage.py migrate
 exec "$@"
--- glossy-gorillas/glossy_gorillas/settings.py
+++ glossy-gorillas/glossy_gorillas/settings.py
@@ -48,6 +48,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "glossy_gorillas.urls"
@@ -110,10 +113,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Login urls
 LOGIN_REDIRECT_URL = "dashboard"
